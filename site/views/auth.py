0cbfd42ca7aa7f2dbf9a3c2b6bcc2b8dd4e1fffa
gzxultra <mygladfinger@gmail.com>
2016-09-03 01:57:17 +0800

    add logout

--- site/views/auth.py
+++ site/views/auth.py
@@ -1,10 +1,10 @@
 # coding: utf-8
-from flask import request
+from flask import request, flash
 from views import main_bp
 from lib.render import ok, error
 from lib.ip import get_ip_by_request
 from models.user import User
-from flask_login import login_user
+from flask_login import login_user, login_required, logout_user
 
 
 @main_bp.route('/auth/sign-up.json', methods=['POST'])
@@ -14,7 +14,7 @@ def sign_up():
     password = request.form.get('password').strip()
     reg_ip = get_ip_by_request(request)
     User.create(email=email, name=name, password=password, reg_ip=reg_ip)
-    return ok('sign up success')
+    return ok('sign up successfully')
 
 
 @main_bp.route('/auth/login.json', methods=['POST'])
@@ -28,4 +28,12 @@ def login():
         return error('wrong password')
 
     login_user(u)
-    return ok('login success')
+    return ok('login successfully')
+
+
+@main_bp.route('/auth/logout.json', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logout.')
+    return ok('logout successfully')
